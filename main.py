432e12306c9a6fab678a10014c380fec27df639b
Vishal Gaddam <41083456+vishalgaddam873@users.noreply.github.com>
2023-12-01 14:17:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+import hashlib
+
+# Function to get the hash string from input string
+def generateHash(inputString):
+    hashObject = hashlib.sha256()
+    hashObject.update(inputString.encode('utf-8'))
+    hashValue = hashObject.hexdigest()
+    return hashValue
+
+
+def main():
+    # Reading and loading json file ("wifi.json")
+    
+
+    # Brut force attack to get password
+    
+
+        # Reading the wordlist to perfrom Dictionary Attack
+        
+        
+        
+        # Iterate the loop on words list to get the valid password
+        
+            
+            # Generate the word hash
+           
+
+            # Match the word hash with the password hash
+           
+    pass
+
+main()
